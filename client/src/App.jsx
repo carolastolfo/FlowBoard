51a42c15e6d583281913b8c4ee447fce89ad9c26
carolastolfo <carolastolfos@gmail.com>
2025-03-07 21:56:24 -0500

    add: React Routing Homepage Login Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import KanbanBoard from './components/KanbanBoard';
 import Boards from './components/Boards';
+import Homepage from './components/Homepage';
+import Login from './components/Login';
+import Register from './components/Register';
 import './App.css'; // Import the global styles
 
 function App() {
   return (
-    <Boards />
-    // <div className='kanban-board'>
-    //   <KanbanBoard />
-    // </div>
+    // added client side routing with react router dom
+    <Router> 
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/boards" element={<Boards/>} />
+      <Route path="/board" element={<KanbanBoard/>} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+  </Router>
   );
 }
 
 export default App;
+
